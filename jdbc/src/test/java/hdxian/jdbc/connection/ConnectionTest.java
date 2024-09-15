1fee1ac5dfe36fc7814221c7537cdf97675ff33b
hdXian <jason123325@gmail.com>
2024-09-16 00:49:27 +0900

    #3 2 - using ConnectionPool as DataSource (HikariCP)

--- jdbc/src/test/java/hdxian/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hdxian/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hdxian.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -34,7 +35,7 @@ public class ConnectionTest {
     }
 
     @Test
-    void dataSourceDriverManager() throws SQLException {
+    void dataSourceDriverManager() throws SQLException, InterruptedException {
         // DriverManagerDataSource: implements Datasource
         // public DriverManagerDataSource(url, username, password)
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
@@ -42,9 +43,26 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+//        dataSource.setMaximumPoolSize(1); // after set MaximumPoolSize 1, we can see that an exception(SQLTransientConnectionException) occurs.
+        dataSource.setPoolName("My Pool");
 
-    private void useDataSource(DataSource dataSource) throws SQLException {
+        useDataSource(dataSource);
+        Thread.sleep(1000); // wait for add Connection on pool thread (for view log)
+    }
+
+
+    private void useDataSource(DataSource dataSource) throws SQLException, InterruptedException {
+
         Connection con1 = dataSource.getConnection();
+//        Thread.sleep(1000);
+//        con1.close(); // must have to call close() after use a Connection (just for test)
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
